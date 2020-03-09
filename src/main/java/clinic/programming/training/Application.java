33383e79350036b259155dd642fa54f3d36cafdd
Senthil Prasanth K <senthilprasanth@gmail.com>
2020-03-09 16:21:16 +0530

    Compiler version changed to 1.8

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,19 @@
 package clinic.programming.training;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
+	public void greet() {
+		List<String> greetings = new ArrayList<>();
+		greetings.add("Hello");
+		
+		for (String greeting : greetings) {
+			System.out.println("Greeting: " + greeting);
+		}
+	}
+		
     public Application() {
         System.out.println ("Inside Application for V2.0");
     }
@@ -10,5 +22,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application V2.0");
 		Application app = new Application();
+		app.greet();
     }
 }
