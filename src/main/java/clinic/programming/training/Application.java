8f9e4d22362f5bc4ff8362bbdbf1db74cbe58996
Senthil Prasanth K <senthilprasanth@gmail.com>
2020-03-09 15:38:20 +0530

    Adding Version 2.0

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -3,12 +3,12 @@ package clinic.programming.training;
 public class Application {
     
     public Application() {
-        System.out.println ("Inside Application");
+        System.out.println ("Inside Application for V2.0");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
+    	System.out.println ("Starting Application V2.0");
 		Application app = new Application();
     }
 }
